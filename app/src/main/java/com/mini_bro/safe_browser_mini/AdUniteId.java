f80a73a7d10495e1f2f02ad29bc87814f1bc14ac
Anam <anamm6650@gmail.com>
2023-08-14 17:40:15 +0100

    This is My 3rd App . This is a Browser App .

---
+++ app/src/main/java/com/mini_bro/safe_browser_mini/AdUniteId.java
@@ -0,0 +1,19 @@
+package com.mini_bro.safe_browser_mini;
+
+import com.atikulsoftware.metaadslibrary.MetaAds.AdsUnit;
+
+public class AdUniteId {
+
+    public static void FbAdId(){
+
+        AdsUnit.BANNER = "239214605682736_239215699015960";
+
+
+
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/mini_bro/safe_browser_mini/MainActivity.java
@@ -0,0 +1,167 @@
+package com.mini_bro.safe_browser_mini;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.atikulsoftware.metaadslibrary.MetaAds.FacebookAds;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.RequestConfiguration;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText urlInput;
+    ImageView clearUrl;
+    WebView webView;
+    ProgressBar progressBar;
+
+
+    public static int CategoryClicked = -10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        urlInput = findViewById(R.id.url_input);
+        clearUrl = findViewById(R.id.clear_icon);
+        progressBar = findViewById(R.id.progress_bar);
+        webView = findViewById(R.id.web_view);
+
+
+        AdUniteId.FbAdId();
+        FacebookAds.loadInterstitial(MainActivity.this);
+
+        FacebookAds.setBanner(findViewById(R.id.banner_container), MainActivity.this);
+
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
+
+        webView.setWebViewClient(new MyWebViewClient());
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                progressBar.setProgress(newProgress);
+            }
+        });
+
+        loadMyUrl("google.com");
+
+        urlInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if(i == EditorInfo.IME_ACTION_GO || i == EditorInfo.IME_ACTION_DONE){
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(urlInput.getWindowToken(),0);
+                    loadMyUrl(urlInput.getText().toString());
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        clearUrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                urlInput.setText("");
+            }
+        });
+
+
+
+
+
+    }
+
+    void loadMyUrl(String url){
+        boolean matchUrl = Patterns.WEB_URL.matcher(url).matches();
+        if(matchUrl){
+            webView.loadUrl(url);
+        }else{
+            webView.loadUrl("google.com/search?q="+url);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else{
+            super.onBackPressed();
+        }
+
+    }
+
+    class MyWebViewClient extends WebViewClient{
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return false;
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            urlInput.setText(webView.getUrl());
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+
+
+    private void initAdmobAd(){
+
+        if (getString(R.string.device_id).length()>12){
+            //Adding your device id -- to avoid invalid activity from your device
+            List<String> testDeviceIds = Arrays.asList(getString(R.string.device_id));
+            RequestConfiguration configuration =
+                    new RequestConfiguration.Builder().setTestDeviceIds(testDeviceIds).build();
+            MobileAds.setRequestConfiguration(configuration);
+        }
+
+
+
+
+        //Init Admob Ads
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+}}
